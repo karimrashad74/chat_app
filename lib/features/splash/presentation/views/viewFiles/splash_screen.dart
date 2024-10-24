a0fa4088a27cb9d72dde502c6570aabd2f74ae51
Karim Rashad <karimrashad.official@gmail.com>
2024-10-24 15:44:50 +0300

    edit core

--- lib/features/splash/presentation/views/viewFiles/splash_screen.dart
+++ lib/features/splash/presentation/views/viewFiles/splash_screen.dart
@@ -1,9 +1,11 @@
-import 'package:chat/core/themes/assets/asset_images.dart';
+import 'package:chat/core/constants/asset_images.dart';
 import 'package:chat/features/auth/presentation/views/viewFiles/login_screen.dart';
 import 'package:chat/features/splash/presentation/views/widgets/navigate_method.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -12,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    customNavigateTimer(context, 3, LoginScreen());
+    customNavigateTimer(context, 3, const LoginScreen());
   }
 
   @override
